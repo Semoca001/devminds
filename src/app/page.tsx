59e0ee4b4bde62029184e8d466119a28db110003
ItsEnder <sebasmorea2003@gmail.com>
2025-05-02 13:19:34 -0500

    Refactor: re organizacion preliminal del proyecto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Title from "@/app/components/sections/Title";
-import AboutUs from "@/app/components/sections/AboutUs";
+import Title from "@/app/components/ui/Title";
+import AboutUs from "@/app/components/sections/Hero";
 
 const Page = () => {
   return (
