ae427e7ccd3f4b02eecb184104a38d9b7f8535a0
Sebastian Morea Cañon <semoca00@gmail.com>
2025-06-21 15:57:50 -0500

    Update Footer.tsx

--- src/app/components/layout/Footer/Footer.tsx
+++ src/app/components/layout/Footer/Footer.tsx
@@ -2,7 +2,7 @@
 
 const Footer = () => {
   return (
-    <footer className="bg-secondary text-main border-t-2 border-footer shadow-sm mt-auto">
+    <footer className="footer-bg border-footer shadow-sm mt-auto">
       <div className="container-main">
         <div className="py-3 sm:py-4 md:py-5">
           <p className="text-center font-doto font-medium text-small text-main tracking-wide">
